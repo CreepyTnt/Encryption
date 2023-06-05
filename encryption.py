b39e030878ad74ebea38b14c80cba4a0a98d4122
CreepyTnt <126629157+CreepyTnt@users.noreply.github.com>
2023-06-04 22:04:14 -0500

    Add files via upload

--- encryption.py
+++ encryption.py
@@ -6,13 +6,21 @@ fernet = Fernet(key)
 
 
 def save_key(location):
-    with open(os.path.join(location, 'key.txt'), 'wb') as f:
-        f.write(key)
+    if os.path.isdir(location):
+        with open(os.path.join(location, 'key.txt'), 'wb') as f:
+            f.write(key)
+    elif os.path.isfile(location):
+        with open(location, 'wb') as f:
+            f.write(key)
+    else:
+        raise ValueError(f'path: "{location}" is not a valid path')
 
-def load_key(location):
+def load_key_from_file(location):
     with open(location, 'rb') as f:
         fernet = Fernet(f.read())
 
+def import_key(key):
+    fernet = Fernet(key)
 
 def import_key(input_key):
     fernet = Fernet(input_key)
